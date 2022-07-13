787b015fb90fdb896cfe39a15686df18c0b77881
Nir Goldman <refaelgold@gmail.com>
2022-07-13 17:00:04 +0300

    Add Col Row information

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import NavbarComponent from "./Navigation/NavbarComponent.js";
 import Table from "./Components/Table.js";
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
@@ -9,12 +11,11 @@ function App() {
         <header>
           <NavbarComponent />
         </header>
-
             <Container>
-            <section className="row mt-4 mb-4">
+            <Row className="row mt-4 mb-4">
                 <h1 className="text-primary fw-bold font-monospace">Lorem Ipsum</h1>
-            </section>
-            <section className="row mt-4 mb-4">
+            </Row>
+            <Row className="mt-4 mb-4">
                 <p className="text-secondary">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
@@ -24,20 +25,22 @@ function App() {
                 <p className="text-secondary">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
-            </section>
+            </Row>
         </Container>
-
-            <Container>
-                <section className="row">
-                    <section className="col-6">
-                        <Table />
-                    </section>
-                    <section className="col-6">
-                        {/*Silence is Golden*/}
-                    </section>
-                </section>
-            </Container>
-        </body>
+        <Container>
+            <Row>
+                <Col>
+                    {/*Silence is Golden*/}
+                </Col>
+                <Col>
+                    <Table />
+                </Col>
+                <Col>
+                    {/*Silence is Golden*/}
+                </Col>
+            </Row>
+        </Container>
+    </body>
   );
 }
 
