0624246ceaf248d481cc07d009a3826b0bbd509a
Nir Goldman <refaelgold@gmail.com>
2022-07-13 16:36:42 +0300

    Add React Nav components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
-import Navbar from "./Navigation/Navbar.js";
+import NavbarComponent from "./Navigation/NavbarComponent.js";
 import Table from "./Components/Table.js";
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
-      <div>
+      <body>
         <header>
-          <Navbar />
+          <NavbarComponent />
         </header>
-        <body>
-        <div className="container">
-            <div className="row mt-4 mb-4">
+
+            <Container>
+            <section className="row mt-4 mb-4">
                 <h1 className="text-primary fw-bold font-monospace">Lorem Ipsum</h1>
-            </div>
-            <div className="row mt-4 mb-4">
+            </section>
+            <section className="row mt-4 mb-4">
                 <p className="text-secondary">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
@@ -23,25 +24,20 @@ function App() {
                 <p className="text-secondary">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
-            </div>
-        </div>
+            </section>
+        </Container>
 
-        <div></div>
-        <div></div>
-        <div className="container">
-            <div className="row">
-            <div className="col-6">
-                <Table />
-            </div>
-            <div className="col-6">
-                {/*Silence is Golden*/}
-            </div>
-            </div>
-
-        </div>
-
+            <Container>
+                <section className="row">
+                    <section className="col-6">
+                        <Table />
+                    </section>
+                    <section className="col-6">
+                        {/*Silence is Golden*/}
+                    </section>
+                </section>
+            </Container>
         </body>
-      </div>
   );
 }
 
