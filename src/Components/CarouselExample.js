c0bff894fa7507ec010d60bdc78d5a8f4cd4b3a5
Nir Goldman <refaelgold@gmail.com>
2022-07-18 16:55:38 +0300

    Translate Carousel Example

--- src/Components/CarouselExample.js
+++ src/Components/CarouselExample.js
@@ -1,43 +1,43 @@
 import Carousel from 'react-bootstrap/Carousel';
+import {useTranslation} from "react-i18next";
 
 const CarouselExample = () => {
+    const {t} = useTranslation();
     return (
         <Carousel>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
                     src="http://via.placeholder.com/800x400?text=First slide&bg=373940"
-                    alt="First slide"
+                    alt={t("First slide")}
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h3>{t("First slide label")}</h3>
+                    <p>{t("Nulla vitae elit libero, a pharetra augue mollis interdum.")}</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
                     src="http://via.placeholder.com/800x400?text=Second slide&bg=373940"
-                    alt="Second slide"
+                    alt={t("Second slide")}
                 />
 
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h3>{t("Second slide label")}</h3>
+                    <p>{t("Nulla vitae elit libero, a pharetra augue mollis interdum.")}</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
                     src="http://via.placeholder.com/800x400?text=Third slide&bg=373940"
-                    alt="Third slide"
+                    alt={t("Third slide")}
                 />
 
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
+                    <h3>{t("Third slide label")}</h3>
+                    <p>{t("Nulla vitae elit libero, a pharetra augue mollis interdum.")}</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
