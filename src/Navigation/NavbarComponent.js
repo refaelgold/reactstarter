4da7c2fa4a607584d7c1559e80755bb5b3c11b13
Nir Goldman <refaelgold@gmail.com>
2022-07-18 12:39:24 +0300

    Add new lang translate with i18n-next

--- src/Navigation/NavbarComponent.js
+++ src/Navigation/NavbarComponent.js
@@ -3,12 +3,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {useTranslation} from "react-i18next";
 
 const NavbarComponent = () => {
+    const {t} = useTranslation();
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">{t("React-Bootstrap")}</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ThemeProvider from 'react-bootstrap/ThemeProvider';
 
+// import i18n (needs to be bundled ;))
+import './i18n';//GLOBAL CALL
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
