6a8f3d010a82c1120fec03acd22ebbc9a0745f7c
Nir Goldman <refaelgold@gmail.com>
2022-07-20 10:25:08 +0300

    Fix Menu to right

--- src/Navigation/NavbarComponent.js
+++ src/Navigation/NavbarComponent.js
@@ -10,12 +10,15 @@ const NavbarComponent = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">{t("React-Bootstrap")}</Navbar.Brand>
+
+                <Navbar.Brand href="#home"><img src='images/logo.png' height={50}/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">{t("Home")}</Nav.Link>
-                        <Nav.Link href="#link">{t("Link")}</Nav.Link>
+                        {/*<Nav.Link href="#home">{t("Home")}</Nav.Link>*/}
+                        <Nav.Link href="#link">{t("Link")}1</Nav.Link>
+                        <Nav.Link href="#link">{t("Link")}2</Nav.Link>
+                        <Nav.Link href="#link">{t("Link")}3</Nav.Link>
                         {/*<NavDropdown title="Dropdown" id="basic-nav-dropdown">*/}
                         {/*    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>*/}
                         {/*    <NavDropdown.Item href="#action/3.2">*/}
