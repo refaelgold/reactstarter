29d1f9790b076c318a5f764f807528c94fc4e660
Nir Goldman <nir@whiteweb.co.il>
2022-08-14 16:11:53 +0300

    Create modules  with Interface (very basic)

--- src/Components/FirstComponent.tsx
+++ src/Components/FirstComponent.tsx
@@ -1,13 +1,15 @@
+import Container from "react-bootstrap/Container";
+
 let Logo ="https://cdn.sanity.io/images/mr2nzgx7/production/1edb8cf5c33875380b28262604bdd31586d78b30-150x32.svg?w=150&h=32&auto=format";
 
 const FirstComponent = () => {
         return (
-            <div>
+            <Container>
                 <h3>בדיקה עבור קומפוננטה פשוטה עם TypeScript</h3>
-                <img height="250" src={Logo} width="250" />
+                <img height="100" src={Logo} width="250" />
                 <p>הקומפוננטה הזאת מראה את הלוגו של Rocket Log</p>
                 <p>לדוגמאות נוספות , יש להכנס לאתר של Rocket Log</p>
-            </div>
+            </Container>
         );
 
 }
--- src/Pages/LogRocket.tsx
+++ src/Pages/LogRocket.tsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import {useTranslation} from "react-i18next";
 import FirstComponent from "../Components/FirstComponent"
+import UserComponent from '../Components/UserComponent'
 
 const LogRocket = () => {
     const {t} = useTranslation();
@@ -12,6 +13,9 @@ const LogRocket = () => {
                 </Row>
                 <Row>
                     <FirstComponent/>
+                </Row>
+                <Row>
+                    <UserComponent name="John Doe" age={26} address="87 Summer St, Boston, MA 02110" dob={new Date()} />
                 </Row>
              </Container>
 
