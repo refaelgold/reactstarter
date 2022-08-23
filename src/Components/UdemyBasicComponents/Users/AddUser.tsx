f4ffb70fd4efc47e53fdcd7b3c648e3659b7751e
Nir Goldman <nir@whiteweb.co.il>
2022-08-23 13:37:47 +0300

    add Card container with special attributes according to React Bootstrap

--- src/Components/UdemyBasicComponents/Users/AddUser.tsx
+++ src/Components/UdemyBasicComponents/Users/AddUser.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import {useTranslation} from "react-i18next";
+import Card from 'react-bootstrap/Card';
+import classes from "./Card.module.css";
 
 
 const AddUser = (props:any) => {
@@ -10,19 +12,26 @@ const AddUser = (props:any) => {
         event.preventDefault();
     }
     return (
-        <Form  onSubmit={addUserHandler}>
-            <Form.Group className="mb-3" controlId="formBasicUserName">
-                <Form.Label>משתמש</Form.Label>
-                <Form.Control type="text" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicAge">
-                <Form.Label htmlFor='age'>גיל</Form.Label>
-                <Form.Control id='age' type="number" />
-            </Form.Group>
-            <Button variant="primary" type="submit" >
-                {t('Add User')}
-            </Button>
-        </Form>
+        <Card className={classes.card}>
+            <Card.Img variant="top" src="https://placeimg.com/640/480/any" />
+            <Card.Body>
+                <Card.Title>כותרת הכרטיס</Card.Title>
+                <Form  onSubmit={addUserHandler}>
+                    <Form.Group className="mb-3" controlId="formBasicUserName">
+                        <Form.Label>משתמש</Form.Label>
+                        <Form.Control type="text" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicAge">
+                        <Form.Label htmlFor='age'>גיל</Form.Label>
+                        <Form.Control id='age' type="number" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" >
+                        {t('Add User')}
+                    </Button>
+                </Form>
+            </Card.Body>
+            {props.children}
+        </Card>
     );
 };
 
