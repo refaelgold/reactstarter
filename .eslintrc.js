037c9a00225484b78eddaa83ad86b6008ec8bff7
Nir Goldman <nirg@moonsite.co.il>
2022-10-13 15:44:44 +0300

    Add rslintrc.js file with Moonsite career config

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  root: true,
+  extends: 'react-app',
+  plugins: ['import'],
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'jsx-a11y/anchor-is-valid': ['error', {
+          'components': [ 'Link' ],
+          'specialLink': [ 'to' ]
+        }],    
+        'prettier/prettier': 0,
+        'quotes': [1, 'single', 'avoid-escape'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'no-duplicate-imports': 'error',
+        'no-const-assign': 'error',
+        'no-var': 'error',
+        'no-multiple-empty-lines': 'error',
+        'sort-imports': [
+          'warn', {
+            'ignoreCase': true,
+            'ignoreDeclarationSort': true,
+          },
+        ],
+        'import/order': [
+          'warn', {
+            'groups': ['builtin', 'external', 'internal'],
+            'pathGroups': [
+              {
+                'pattern': 'react*',
+                'group': 'external',
+                'position': 'before',
+              },
+              {
+                'pattern': '@*',
+                'group': 'external',
+                'position': 'after',
+              },
+              {
+                'pattern': '@*/**',
+                'group': 'external',
+                'position': 'after',
+              },
+              {
+                'pattern': 'assets/**',
+                'group': 'internal',
+                'position': 'after',
+              },
+            ],
+            'pathGroupsExcludedImportTypes': [],
+            'alphabetize': {
+              'order': 'asc',
+              'caseInsensitive': true,
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
